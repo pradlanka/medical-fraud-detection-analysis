4a63b44508da57f2dd1b289f492ae9f8c088b3b9
pradlanka <pradyulanka@gmail.com>
2023-11-26 20:18:00 -0500

    updated float in app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with col1:
 with col2:
     st.text("OP characteristics")
     OP_TotalClaims = st.number_input('OP_TotalClaims', min_value=0, max_value=40)
-    OP_RenalDiseaseIndicator =st.number_input('OP_RenalDiseaseIndicator', min_value=0, max_value=1, format="%.2f", step=0.01)
+    OP_RenalDiseaseIndicator =st.number_input('OP_RenalDiseaseIndicator', min_value=0.00, max_value=1.00, format="%.2f", step=0.01)
     OP_NoClmDiagnosisCodes  = st.number_input('OP_NoClmDiagnosisCodes', min_value=0, max_value=20)
 
 st.text('')
