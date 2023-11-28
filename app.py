f18fe4e35d6cfd1243e4630566eda45caaa289fe
pradlanka <pradyulanka@gmail.com>
2023-11-27 19:58:46 -0500

    Cleaned

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ st.markdown('Model to classify fradulent medical providers')
 st.header("Provider features")
 col1, col2 = st.columns(2)
 with col1:
-    st.text("IP characterisitcs")
+    st.text("In-patient characterisitcs")
     IP_days_in_hospital = st.number_input('Average stay of in-patient claims', min_value=0, max_value=40)
     IP_TotalClaims = st.number_input('Total no of in-patient claims filed by the provider', min_value=0, max_value=500)
 with col2:
-    st.text("OP characteristics")
+    st.text("Out-patient characteristics")
     OP_TotalClaims = st.number_input('Total no of out-patient claims filed by the provider', min_value=0, max_value=40)
     OP_RenalDiseaseIndicator =st.number_input('Fraction of patients with renal disease in Outpatient claims', min_value=0.00, max_value=1.00, format="%.2f", step=0.01)
     OP_NoClmDiagnosisCodes  = st.number_input('Total no of outpatient diganosis codes in claims filed by the provider', min_value=0, max_value=20)
